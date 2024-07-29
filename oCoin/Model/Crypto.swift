a510a8b4a8d234a0a39e4235d343604194228e59
Fatih Oguz <oguzfatih39@gmail.com>
2024-07-29 14:41:37 +0300

    Crypto App is started

---
+++ oCoin/Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  oCoin
+//
+//  Created by Fatih Oğuz on 29.07.2024.
+//
+
+import Foundation
+
+struct Crypto : Decodable{
+    let currency : String
+    let price : String
+}
---
+++ oCoin/View/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  oCoin
+//
+//  Created by Fatih Oğuz on 28.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        content.text = "Crypto App"
+        content.secondaryText = "Crypto Price"
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
